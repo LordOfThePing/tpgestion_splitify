f086571fc60a7e6c3115d36e3a5f1bd0b87e0e14
Agustina Schmidt <aschmidt@tdi.com.ar>
2024-05-08 19:44:44 -0300

    fix componente guia

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { BackendService } from '../services/backend/backend.service';
 import { NgIf } from '@angular/common';
-import { User } from '../services/auth.service';
+import { User } from '../../classes/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  imports: [NgIf], 
+  imports: [NgIf],
   templateUrl: './hello.component.html',
 })
 export class HelloComponent {
   //responseContent es un parámetro que se puede utilizar en la template asociada al componente
   responseContent: Array<User>;
 
-  constructor(private backendService: BackendService) { 
+  constructor(private userService: UserService) {
     this.responseContent = new Array;
   }
 
-   /**
-   * Initializes the component and retrieves the hello message from the backend service.
-   *
-   * @return {void} This function does not return anything.
-   */
+  /**
+  * Initializes the component and retrieves the hello message from the backend service.
+  *
+  * @return {void} This function does not return anything.
+  */
   ngOnInit(): void {
-    this.backendService.getUsers().subscribe(
+    this.userService.getUsers().subscribe(
       (data: Array<User>) => {
-        
+
         this.responseContent = data;
         console.log(data);
       },
