8e7f89fe665c9a274a9661f01df53197d6b920d3
Agustina Schmidt <aschmidt@tdi.com.ar>
2024-05-08 19:44:58 -0300

    logout

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +15,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 export class HeaderComponent {
   @Output() sidebarEvent = new EventEmitter<any>();
 
+  constructor(private authService: AuthService, private router: Router){}
+
   toggleSidebar(){
     this.sidebarEvent.emit();
+  }
+
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
