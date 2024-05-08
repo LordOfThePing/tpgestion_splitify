dd6f365ff6704ccf5297c8682511d0e7292e17bf
juan518munoz <62400508+juan518munoz@users.noreply.github.com>
2024-05-08 15:18:32 -0300

    initial commit (#1)
    Co-authored-by: Pedro Flynn <86746793+LordOfThePing@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -6,8 +7,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    HttpClientModule
+    RouterOutlet,
+    HttpClientModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public userGroups: any;
+  public groupName: string = "";
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://0.0.0.0:8000/user_groups').subscribe(data => {
+      this.userGroups = data;
+      console.log("User groups: ", this.userGroups);
+    });
+  }
+
+  createGroup(): void {
+    console.log("create group")
+    const groupData = {
+      name: this.groupName
+    };
+
+    this.http.post('http://0.0.0.0:8000/user_groups', groupData).subscribe(response => {
+      console.log(response);
+      this.groupName = ''; // Clear the input field
+
+      // Refresh the user groups
+      this.http.get('http://0.0.0.0:8000/user_groups').subscribe(data => {
+        this.userGroups = data;
+        console.log("User groups: ", this.userGroups);
+      });
+    });
+  }
+
+  editGroup(): void {
+    console.log("edit group")
+  }
+
+  deleteGroup(): void {
+    console.log("delete group")
+  }
 }
