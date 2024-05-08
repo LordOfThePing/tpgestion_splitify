7a32eb19393a985f94b38956688182bebb27efbc
Pedro Andrés Flynn <pedro.flynn@middlesoft.com.uy>
2024-05-08 15:34:24 -0300

    fix ip address

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,11 +16,12 @@ import { FormsModule } from '@angular/forms';
 export class HomeComponent implements OnInit {
   public userGroups: any;
   public groupName: string = "";
+  private apiUrl = 'http://localhost:8000'; // URL de tu backend FastAPI
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('http://0.0.0.0:8000/user_groups').subscribe(data => {
+    this.http.get(`${this.apiUrl}/user_groups`).subscribe(data => {
       this.userGroups = data;
       console.log("User groups: ", this.userGroups);
     });
@@ -32,12 +33,12 @@ export class HomeComponent implements OnInit {
       name: this.groupName
     };
 
-    this.http.post('http://0.0.0.0:8000/user_groups', groupData).subscribe(response => {
+    this.http.post(`${this.apiUrl}/user_groups`, groupData).subscribe(response => {
       console.log(response);
       this.groupName = ''; // Clear the input field
 
       // Refresh the user groups
-      this.http.get('http://0.0.0.0:8000/user_groups').subscribe(data => {
+      this.http.get(`${this.apiUrl}/user_groups`).subscribe(data => {
         this.userGroups = data;
         console.log("User groups: ", this.userGroups);
       });
